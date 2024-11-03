6a465ed270585d0b0f838f7a574a28b42102976d
Guilherme Bacelar dos Santos Soares <bacelarguilherme7@gmail.com>
2024-11-03 16:25:58 -0300

    first commit

---
+++ src/app/components/add-lista/add-lista.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-lista.component.html',
+  styleUrl: './add-lista.component.scss'
+})
+export class AddListaComponent {
+
+
+  onSubmit() {
+    
+  }
+
+}
